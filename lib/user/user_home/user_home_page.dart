8b2cf36c71d4b3edb99d3458acf77a1fc8da6719
FarahhRahmeh <farahh.rahmeh@gmail.com>
2024-05-14 11:00:49 -0700

    🔔Added the notification icon to the appbar in the user home page

--- lib/user/user_home/user_home_page.dart
+++ lib/user/user_home/user_home_page.dart
@@ -3,6 +3,7 @@ import 'package:booktaste/common/widgets/custom_shapes/Containers/primary_header
 import 'package:booktaste/utils/constans/colors.dart';
 import 'package:booktaste/utils/constans/texts.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class UserHomePage extends StatelessWidget {
   const UserHomePage({super.key});
@@ -18,19 +19,36 @@ class UserHomePage extends StatelessWidget {
                 title: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(Texts.homeAppbarTitle,
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelMedium!
-                            .apply(color: MyColors.lightGrey)),
-                    Text(Texts.homeAppbarSubTitle,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall!
-                            .apply(color: MyColors.white)),
+                    Text(
+                      Texts.homeAppbarTitle,
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelMedium!
+                          .apply(color: MyColors.lightGrey),
+                    ),
+                    Text(
+                      Texts.homeAppbarSubTitle,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall!
+                          .apply(color: MyColors.white),
+                    ),
                   ],
                 ),
-              )
+                actions: [
+                  Stack(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Iconsax.notification,
+                          //! here you can change the icon color
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ],
           ),
         ),
