ebdd055e115925e6f67cf83b98a2d3578560ef5b
FarahhRahmeh <farahh.rahmeh@gmail.com>
2024-05-19 13:02:03 -0700

    🖼Added user profile files - profile menu (items) - Note: just a UI now

--- lib/user/user_profile/user_profile.dart
+++ lib/user/user_profile/user_profile.dart
@@ -1,8 +1,12 @@
 import 'package:booktaste/common/widgets/appbar/appbar.dart';
 import 'package:booktaste/common/widgets/image/circular_image.dart';
+import 'package:booktaste/common/widgets/texts/section_heading.dart';
+import 'package:booktaste/user/user_profile/user_profile_widgets/profile_menu.dart';
+import 'package:booktaste/utils/constans/colors.dart';
 import 'package:booktaste/utils/constans/images.dart';
 import 'package:booktaste/utils/constans/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class UserProfile extends StatelessWidget {
   const UserProfile({super.key});
@@ -19,9 +23,79 @@ class UserProfile extends StatelessWidget {
           child: Column(
             children: [
               //!Profile Picture
-              CircularImage(
-                image: Images.onboarding_1,
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    CircularImage(
+                      image: Images.onboarding_1,
+                      width: 80,
+                      height: 80,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Change Picture'),
+                    )
+                  ],
+                ),
               ),
+              //!Details
+              const SizedBox(
+                height: Sizes.spaceBtwItems / 2,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: Sizes.spaceBtwItems,
+              ),
+              SectionHeading(
+                title: 'Profile Inforamtion',
+                showActionButton: false,
+              ),
+              const SizedBox(
+                height: Sizes.spaceBtwItems,
+              ),
+
+              ProfileMenu(
+                  title: 'Name :', value: 'UserName Here', onPressed: () {}),
+
+              const SizedBox(
+                height: Sizes.spaceBtwItems,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: Sizes.spaceBtwItems,
+              ),
+              SectionHeading(
+                title: 'Personal Inforamtion',
+                showActionButton: false,
+              ),
+              const SizedBox(
+                height: Sizes.spaceBtwItems,
+              ),
+              ProfileMenu(
+                  title: 'Email :', value: 'E-mail Here', onPressed: () {}),
+              ProfileMenu(
+                title: 'UserId :',
+                value: '22212',
+                onPressed: () {},
+                icon: Iconsax.copy_copy,
+              ),
+
+              //todo more examples for ProfileMenu:   userId, Gender, phone ,date of birth ....
+
+              const Divider(),
+              const SizedBox(
+                height: Sizes.spaceBtwItems,
+              ),
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'close account',
+                    style: TextStyle(color: pinkish),
+                  ),
+                ),
+              )
             ],
           ),
         ),
